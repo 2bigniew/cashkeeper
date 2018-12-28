d68f04daa7f07c8d474d92a5c4cc300c3cf2be02
root <stasiakzb@gmail.com>
2018-12-28 21:02:13 +0100

    Correction new user creating - add user to db suced adding user to database

---
+++ Backend/Passport/local.js
@@ -0,0 +1,71 @@
+module.exports = function(passport, user) {
+ 
+    const User = user;
+    const LocalStrategy = require('passport-local').Strategy;
+
+    passport.serializeUser(function(user, done) {
+        done(null, user.user_id);
+    });
+
+    passport.deserializeUser(function(id, done) {
+        User.findByPk(id).then(function(user) {
+            if (user) {
+                done(null, user.get());
+            } else {
+                done(user.errors, null);
+            }
+        });
+    });
+
+    passport.use('local-create-account', new LocalStrategy({
+        usernameField: 'login',
+        passwordField: 'password',
+        passReqToCallback: true
+    },
+    
+
+    function(req, login, password, done) {
+        // const generateHash = function(password) {
+ 
+        //     return bCrypt.hashSync(password, bCrypt.genSaltSync(8), null);
+
+        // };
+
+        // dlaczego przy create automatycznie nadaje user_id jako NULL ???
+
+        User.findOne({
+            where: {
+                login: login
+            }
+        }).then(user => {
+            if (user) {
+                return done(null, false, {
+                    message: 'That email is already taken' // napisac przekierowanie i wiadomosc we flashu 
+                })
+            } else {
+                const userPassword = password;
+                const data = {
+                    login: login,
+                    password: userPassword,
+                    firstname: req.body.firstname,
+                    lastname: req.body.lastname,
+                    is_deleted: false,
+                    created_at: '2018-12-28 19:24:57',
+                    bank_account: req.body.bank_account,
+                };
+
+                User.create(data).then(function(newUser) {
+                        console.log(newUser);
+                    if (!newUser) {
+                        return done(null, false);
+                    }
+
+                    if (newUser) {
+                        return done(null, newUser);
+                    }
+                });    
+            }
+        })
+    }
+))
+}
