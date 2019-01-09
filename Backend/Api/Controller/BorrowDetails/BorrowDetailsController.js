6675e0171e57bc034578a5f4a76fd872aed15c28
root <stasiakzb@gmail.com>
2019-01-09 21:11:01 +0100

    Route Borrows init

--- Backend/Api/Controller/BorrowDetails/BorrowDetailsController.js
+++ Backend/Api/Controller/BorrowDetails/BorrowDetailsController.js
@@ -1,4 +1,7 @@
 const BorrowDetails = require('../../../Database/Models/BorrowDetails');
+const PartnerAccount = require('../../../Database/Models/PartnerAccount');
+
+const Sequalize = require('sequelize');
 
 exports.getBorrowsDetailsData = async(req, res, next) => {
     const userId = req.session.passport.user;
@@ -7,6 +10,36 @@ exports.getBorrowsDetailsData = async(req, res, next) => {
         where: {
             user_id: userId
         }
+    });
+
+    res.send(borrows);
+}
+
+exports.getBorrowsForPartner = async(req, res, next) => {
+    const userId = req.session.passport.user;
+    const Op = Sequalize.Op;
+
+    PartnerAccount.hasMany(BorrowDetails, {foreignKey: 'user_id'});
+    BorrowDetails.belongsTo(PartnerAccount, {foreignKey: 'user_id'});
+
+    const borrows = await BorrowDetails.findAll({
+        where: {
+            user_id: userId,
+            purpose: {
+                [Op.iLike]: `%${req.params.purpose}%`
+            },
+        }, 
+        include: [{
+            model: PartnerAccount,
+            where: {
+                firstname: {
+                    [Op.iLike]: `%${req.params.firstname}%`
+                },
+                lastname: {
+                    [Op.iLike]: `%${req.params.lastname}%`
+                } 
+            }
+        }]
     });
 
     res.send(borrows);
--- Backend/Api/Routing/BasicData/BorrowDetails.js
+++ Backend/Api/Routing/BasicData/BorrowDetails.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const Helpers = require('../../../Helpers/Helpers');
-
-const BorrowDetailsController = require('../../Controller/BorrowDetails/BorrowDetailsController');
 const errorHandler = require('../../Middleware/error');
 
+const BorrowDetailsController = require('../../Controller/BorrowDetails/BorrowDetailsController');
+
 router.get('/list', Helpers.isLoggedIn, errorHandler.catchAsyncErrors(BorrowDetailsController.getBorrowsDetailsData));
+
+router.get('/find', Helpers.isLoggedIn, errorHandler.catchAsyncErrors(BorrowDetailsController.getBorrowsForPartner));
+
+// lista dla wybranych partnerow oraz z wybranego przedzialu dat
+
+// lista sumy wszystkich
+
+// dodawanie nowej pozyczki
+
+// edytowanie pozyczki
+
+
 
 module.exports = router;
